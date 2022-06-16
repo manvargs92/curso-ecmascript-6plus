2ddeed99f02541ad4aebe5eb582696a84bfc1771
manavrgs92 <manuel.vargas.garrido@outlook.com>
2022-06-15 19:40:05 -0500

    Async Await

--- es8/index.js
+++ es8/index.js
@@ -21,3 +21,32 @@ const string = "Hello";
 console.log(string.padStart(7, "hi")); // .padStart(max caract, letras a agregar) añade mas letras a un string
 console.log(string.padEnd(12, " -----"));
 console.log("food".padEnd(12, " -----"));
+
+// ASYNC AWAIT
+const hellowWorld = function () {
+    return new Promise(function (resolve, reject) {
+        (true) 
+        ? setTimeout(function () {
+            resolve("Hello World");
+        }, 3000)
+        : reject(new Error("Test Error"));
+    });
+};
+
+const helloAsync = async function () {
+    const hello = await hellowWorld();
+    console.log(hello);
+};
+
+helloAsync();
+
+const anotherFunction = async function () {
+    try {
+        const hellow = await hellowWorld();
+        console.log(hellow);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+anotherFunction();
